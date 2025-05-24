027432f2c854e23e3412a5f29c7cfc3e4809a227
Catherine Earl <116440382+RubyCloud225@users.noreply.github.com>
2025-05-24 17:31:52 +0100

    commit change of name

--- ERP/ERP_backend/Attributes/ERPAttribute.cs
+++
@@ -1,9 +0,0 @@
-namespace ERP.Attributes
-{
-    [AttributeUsage(AttributeTargets.Method)]
-    public class ERPAttribute(string AllowedFileTypes, int maxFileSize) : Attribute
-    {
-        public string AllowedFileTypes { get; set; } = AllowedFileTypes;
-        public int MaxFileSize { get; set; } = maxFileSize;
-    }
-}
