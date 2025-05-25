bb3e9d8a8a3404ab18ad8f8994a2bbeeed6847c9
Catherine Earl <116440382+RubyCloud225@users.noreply.github.com>
2025-05-25 20:54:45 +0100

    testing

--- ERP/ERP/obj/Debug/net9.0/ERP.AssemblyInfo.cs
+++ ERP/ERP/obj/Debug/net9.0/ERP.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ERP")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d0112d6c94191b1465a13fbd4adeac4a0a85fdac")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+96bde95667219b913b838069c9566be8bdd1d92b")]
 [assembly: System.Reflection.AssemblyProductAttribute("ERP")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ERP")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
